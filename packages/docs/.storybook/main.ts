5bff6c4689d563a7ab6ed8871a97144cec2cacd8
Lucas Tello <lucastelloweb@gmail.com>
2024-05-06 14:44:43 -0300

    fix: add subpath to Storybook on production

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -26,5 +26,12 @@ const config: StorybookConfig = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if(configType === 'PRODUCTION') {
+      config.base = '/ignite-design-system/'
+    }
+
+    return config
+  }
 };
 export default config;
